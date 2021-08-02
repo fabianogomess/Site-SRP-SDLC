1ea9e13331777e37927d9900e8567be874c8ac47
Fabiano <fabianogomespires@hotmail.com>
2021-08-02 15:45:55 -0300

    rrevisão da estrutura e finalisação da estilização

--- src/script/pessoa.js
+++ src/script/pessoa.js
@@ -1,8 +1,9 @@
 //lista de membros do projeto 
 const renato_bulcao = {
-    nome: "Mr. Renato Bulcao",
+    nome: "Mr. Renato Bulcão",
     imagem: "../img/Group 4 (1).png",
     graduacao: "Computer Science (UFMA)",
+    especializacao:"MIgezendo",
     posGraduacao: "Computer Science and Computational Mathematics (USP)",
     doutorado: "Computer Science and Computational Mathematics (USP)"
 }
@@ -31,6 +32,7 @@ const pollyana_queiroz = {
     nome:"Prof. Pollyana de Queiroz Ribeiro",
     imagem:"../img/Group 4 (1).png",
     graduacao:"Information Systems (UEG), Pedagogy (UNOPAR)",
+    especializacao:"",
     posGraduacao:"Computer Science (UFG)",
     doutorado:""
 }
@@ -60,7 +62,7 @@ const matheus_ribeiro = {
     imagem:"../img/Group 4 (1).png",
     graduacao:"",
     posGraduacao:"",
-    dutorado:""
+    doutorado:""
 }
 const thalia_santos = {
     nome:"Thalia Santos de Santana",
@@ -110,48 +112,63 @@ const pessoas = {
 }
 
 //função responsavel por receber uma pessoa e mostrar a pagina pessoal dela con as informações 
-function procurarPessoa(pessoa){
-    const innerHtml = `
+function procurarPessoa(){
     
-
-    <div class = "tituloPessoa">
-        <h2 class="clickable" onclick="mostrarPessoas()">MEMBERS</h2>
-        <img src="../img/ic_round-navigate-next.png" alt="sinal de maior-que">
-        <h3>${pessoas[pessoa].nome}</h3>
-    </div>
-
-    <div class = "conteudoPessoa">
-        <div class= "coluna1">
-        <img src="${pessoas[pessoa].imagem}" alt="img de perfil da pessoa">
-        </div>
-
-        <div class = "coluna2">
-            <h4>CAREER SUMMARY</h4>
-            
-            <p>Graduation</p>
+    const queryParam = document.location.search;
+    const querySearch = new URLSearchParams(queryParam);
+    const pessoa = querySearch.get("member")
+    
+    let especializacao = "";
+    if (pessoas[pessoa].especializacao){
+        especializacao = `
+        <p>Especialization</p>
             <div class = "info01">
                 <img src="..//img/seta.png" alt="seta">  
-                <p>${pessoas[pessoa].graduacao}</p>
+                <p>${pessoas[pessoa].especializacao}</p>
+            </div>   
+        
+        `
+    }
+    const innerHtml = `
+        <div class = "tituloPessoa">
+            <h2><a id = "ancora" href="pessoas.html">MEMBERS</a></h2>
+            <img src="../img/ic_round-navigate-next.png" alt="sinal de maior-que">
+            <h3>${pessoas[pessoa].nome}</h3>
+        </div>
+
+        <div class = "conteudoPessoa">
+            <div class= "coluna1">
+            <img src="${pessoas[pessoa].imagem}" alt="img de perfil da pessoa">
             </div>
-            
-            <p>MSc</p>
-            <div class = "info02">
-                <img src="..//img/seta.png" alt="seta"> 
-                <p>${pessoas[pessoa].posGraduacao}</p>
-            </div>
-            
-            
-            <p>Phd</p>
-            <div class = "info03">
-                <img src="..//img/seta.png" alt="seta" > 
-                <p>${pessoas[pessoa].doutorado}</p>
+
+            <div class = "coluna2">
+                <h4>CAREER SUMMARY</h4>
+                
+                <p>Graduation</p>
+                <div class = "info01">
+                    <img src="..//img/seta.png" alt="seta">  
+                    <p>${pessoas[pessoa].graduacao}</p>
+                </div>
+                
+                ${especializacao}
+
+                <p>MSc</p>
+                <div class = "info02">
+                    <img src="..//img/seta.png" alt="seta"> 
+                    <p>${pessoas[pessoa].posGraduacao}</p>
+                </div>
+                
+                <p>Phd</p>
+                <div class = "info03">
+                    <img src="..//img/seta.png" alt="seta" > 
+                    <p>${pessoas[pessoa].doutorado}</p>
+                </div>
             </div>
         </div>
-    </div>
 
 
     `
     const conteudoHtml = document.getElementById('conteudo')
-    conteudoHtml.innerHTML = innerHtml;
+    conteudoHtml.innerHTML = innerHtml, "#acnora";
     return true;
 }
